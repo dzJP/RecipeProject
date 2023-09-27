ecc833a91ee8063bdbabfc4e91550ed6f1c73074
dzJP <113700179+dzJP@users.noreply.github.com>
2023-09-27 18:30:17 +0200

    Added search function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,8 @@
     <h1>Home Page</h1>
     <div v-if="loading">Loading...</div>
     <div v-else>
-      <div v-for="recipe in recipes" :key="recipe._id">
+      <input v-model="searchQuery" @input="searchRecipes" placeholder="Search recipes..." />
+      <div v-for="recipe in filteredRecipes" :key="recipe._id">
         <h2>{{ recipe.title }}</h2>
         <img :src="recipe.imageUrl" alt="Recipe Image" class="recipe-image" />
         <div>Rating: {{ recipe.avgRating || 'N/A' }}</div>
@@ -11,12 +12,15 @@
         <div>Time: {{ recipe.timeInMins }} mins</div>
         <div>Category: {{ recipe.categories.join(', ') }}</div>
       </div>
-
-      <!-- Display the list of categories -->
-      <div v-for="category in categories" :key="category.name">
-        <router-link :to="'/category/' + category.name">
-          {{ category.name }} ({{ category.count }} recipes)
-        </router-link>
+      <div v-if="categories.length > 0">
+        <div v-for="category in categories" :key="category.name">
+          <router-link :to="'/category/' + category.name">
+            {{ category.name }} ({{ category.recipeCount }} recipes)
+          </router-link>
+        </div>
+      </div>
+      <div v-else>
+        No categories available.
       </div>
     </div>
   </div>
@@ -29,38 +33,46 @@ export default {
       loading: true,
       recipes: [],
       categories: [],
+      searchQuery: '',
     };
   },
 
+  computed: {
+    uniqueRecipes() {
+      const uniqueTitles = [...new Set(this.recipes.map(recipe => recipe.title))];
+      return uniqueTitles.map(title => this.recipes.find(recipe => recipe.title === title));
+    },
+    filteredRecipes() {
+      return this.uniqueRecipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    },
+  },
+
   created() {
-  fetch('https://jau22-recept-grupp3-j35j900nj4w3.reky.se/recipes')
-    .then(response => response.json())
-    .then(data => {
-      // Remove duplicate entries based on recipe title
-      const uniqueRecipes = Array.from(new Set(data.map(recipe => recipe.title)))
-        .map(title => data.find(recipe => recipe.title === title));
+    fetch('https://jau22-recept-grupp3-j35j900nj4w3.reky.se/recipes')
+      .then(response => response.json())
+      .then(data => {
+        this.recipes = data;
+        this.loading = false;
+      })
+      .catch(error => {
+        console.error('Error fetching recipes:', error);
+        this.loading = false;
+      });
 
-      this.recipes = uniqueRecipes;
-      this.loading = false;
-    })
-    .catch(error => {
-      console.error('Error fetching recipes:', error);
-      this.loading = false;
-    });
-
-  // Fetch the list of categories and the recipe count for each category
-  fetch('https://jau22-recept-grupp3-j35j900nj4w3.reky.se/categories')
-    .then(response => response.json())
-    .then(data => {
-      this.categories = data.map(category => ({
-        name: category.name,
-        count: category.count,
-      }));
-    })
-    .catch(error => {
-      console.error('Error fetching categories:', error);
-    });
-  }
+    fetch('https://jau22-recept-grupp3-j35j900nj4w3.reky.se/categories')
+      .then(response => response.json())
+      .then(data => {
+        this.categories = data.map(category => ({
+          name: category.name,
+          recipeCount: category.count || 0,
+        }));
+      })
+      .catch(error => {
+        console.error('Error fetching categories:', error);
+      });
+  },
 };
 </script>
 
