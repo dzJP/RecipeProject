1c52b2e70cb3256a113ff06bb9af1b2657409e28
Drstocktor <talibanvicke@live.se>
2023-09-29 06:56:40 +0200

    add header and switch from flex to grid

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="grid-box">
+
+    <div class ="header">
+        <h1>Lorem ipsum dolor</h1>
+        <p>Sit amet, consectetur adipisicing elit. Dolorem cupiditate quo rerum hic adipisci, obcaecati architecto eligendi qui dolore veritatis non porro enim repellat magni delectus vero provident aperiam placeat.</p>
+    </div>
+
     <div class="navbar">
       <!-- If categories are available, display them as links -->
       <div v-if="categories.length > 0">
@@ -107,16 +113,21 @@ export default {
   height: 100vh;
   display: grid;
   grid-template-columns: 200px 1fr;
-  grid-template-rows: 30px 1fr;
+  grid-template-rows: 300px 30px 1fr;
+  gap: 1rem;
+}
+
+.grid-box > * {
+  border-radius: 10px;
 }
 
 .recipe-image {
-  width: 400px;
+  width: 250px;
   height: auto;
 }
 .searchbar {
   grid-column: 2 / 3;
-  grid-row: 1 / 2;
+  grid-row: 2 / 3;
 }
 
 .searchbar input {
@@ -124,26 +135,37 @@ export default {
   height: 20px;
 }
 
+.header {
+  text-align: center;
+  background-color: grey;
+  grid-row: 1/2;
+  grid-column: 1/3;
+}
+
 .navbar {
   grid-column: 1 / 2;
-  grid-row: 1 / 3;
+  grid-row: 2 / 4;
   background-color: rgb(145, 148, 158);
 }
 
 .big-recipes-container {
   grid-column: 2 / 3;
-  grid-row: 2 / 3;
+  grid-row: 3 / 4;
 
-  display: flex;
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: auto;
+  gap: 0.5rem;
 
   background-color: rgb(150, 146, 140);
   border: 1px solid black;
 }
 
-.recipe-container {
-  flex-basis: 100%;
+.big-recipes-container > * {
   border-radius: 10px;
   background-color: rgb(183, 211, 207);
   border: 1px solid black;
 }
+
 </style>
