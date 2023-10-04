243706ec094aa68e3f98a8dfe80509d3baf14e0e
dzJP <113700179+dzJP@users.noreply.github.com>
2023-10-04 02:57:59 +0200

    clickable recipes

--- src/components/RecipesContainerComponent.vue
+++ src/components/RecipesContainerComponent.vue
@@ -1,50 +1,53 @@
 <template>
     <div class="big-recipes-container">
-        <div v-for="recipe in filteredRecipes" :key="recipe._id">
-            <h2>{{ recipe.title }}</h2>
-            <img :src="recipe.imageUrl" alt="Recipe Image" class="recipe-image" />
-            <div>Rating: {{ recipe.avgRating || 'N/A' }}</div>
-            <div>Ingredients: {{ recipe.ingredients.length }}</div>
-            <div>Time: {{ recipe.timeInMins }} mins</div>
-            <div>Category: {{ recipe.categories.join(', ') }}</div>
-        </div>
+      <div v-for="recipe in filteredRecipes" :key="recipe._id">
+        <router-link :to="'/recipe/' + recipe._id">
+          <h2>{{ recipe.title }}</h2>
+          <img :src="recipe.imageUrl" alt="Recipe Image" class="recipe-image" />
+        </router-link>
+        <div>Rating: {{ recipe.avgRating || 'N/A' }}</div>
+        <div>Ingredients: {{ recipe.ingredients.length }}</div>
+        <div>Time: {{ recipe.timeInMins }} mins</div>
+        <div>Category: {{ recipe.categories.join(', ') }}</div>
+      </div>
     </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
     props: ['recipes', 'searchQuery'],
     computed: {
-        filteredRecipes() {
-            if (Array.isArray(this.recipes)) {
-                return this.recipes.filter(recipe =>
-                    recipe.title.toLowerCase().includes(this.searchQuery.toLowerCase())
-                );
-            }
-            return [];
-        },
+      filteredRecipes() {
+        if (Array.isArray(this.recipes)) {
+          return this.recipes.filter(recipe =>
+            recipe.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+          );
+        }
+        return [];
+      },
     },
-};
-</script>
-
-<style scoped>
-.big-recipes-container {
+  };
+  </script>
+  
+  <style scoped>
+  .big-recipes-container {
     grid-column: 2 / 3;
     grid-row: 3 / 4;
     display: grid;
     grid-template-columns: 1fr;
     grid-template-rows: auto;
     gap: 0.5rem;
-}
-
-.big-recipes-container>* {
+  }
+  
+  .big-recipes-container > * {
     border-radius: 10px;
     background-color: rgb(183, 211, 207);
     border: 1px solid black;
-}
-
-.recipe-image {
+  }
+  
+  .recipe-image {
     width: 250px;
     height: auto;
-}
-</style>
+  }
+  </style>
+  
