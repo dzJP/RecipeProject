cd724056d10eee8a7ed088c68780ac80662e901a
Drstocktor <talibanvicke@live.se>
2023-10-07 01:43:43 +0200

    add button component with hover

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <router-link :to="to" class="button">
+        <slot></slot>
+    </router-link>
+</template>
+
+<script>
+export default {
+    props: {
+        to: String,
+    },
+};
+</script>
+
+<style>
+.button {
+    background-color: #00A1F1;
+    color: #F3FAFF;
+    border-radius: 10px;
+    text-align: center;
+    border: none;
+    padding: 15px;
+    transition: background-color 0.3s ease;
+}
+
+.button:hover {
+    /* background-color: #6D94BC; */
+    background-color: #6D94BC;
+}
+</style>
